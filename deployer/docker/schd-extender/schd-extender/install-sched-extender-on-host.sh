5d8a1cd065fae4be9fb5d1ddc16ef20cffe59c6c
cheyang <cheyang@163.com>
2019-04-17 17:19:03 +0800

    fix schd deployer

--- deployer/docker/schd-extender/schd-extender/install-sched-extender-on-host.sh
+++ deployer/docker/schd-extender/schd-extender/install-sched-extender-on-host.sh
@@ -19,11 +19,17 @@ public::deployer::sche-policy-config() {
 		public::common::log "Backup $backup_dir/kube-scheduler-$(date +%Y%m%d_%H%M%S).yaml"
 		public::common::log "Configure shceduler extender"
 		cp /usr/local/k8s-schd-extender/scheduler-policy-config.json /etc/kubernetes/scheduler-policy-config.json
-		sed -i "/- kube-scheduler/a\ \ \ \ - --policy-config-file=/etc/kubernetes/scheduler-policy-config.json" $dir/kube-scheduler.yaml
+		if ! grep 'policy-config-file=/etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
+			sed -i "/- kube-scheduler/a\ \ \ \ - --policy-config-file=/etc/kubernetes/scheduler-policy-config.json" $dir/kube-scheduler.yaml
+		fi
 		# add scheduler config policy volumeMounts
-		sed -i "/  volumeMounts:/a\ \ \ \ - mountPath: /etc/kubernetes/scheduler-policy-config.json\n      name: scheduler-policy-config\n      readOnly: true" $dir/kube-scheduler.yaml
+		if ! grep 'mountPath: /etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
+			sed -i "/  volumeMounts:/a\ \ \ \ - mountPath: /etc/kubernetes/scheduler-policy-config.json\n      name: scheduler-policy-config\n      readOnly: true" $dir/kube-scheduler.yaml
+		fi
 		# add scheduler config policy volumes
-		sed -i "/  volumes:/a \  - hostPath:\n      path: /etc/kubernetes/scheduler-policy-config.json\n      type: FileOrCreate\n    name: scheduler-policy-config" $dir/kube-scheduler.yaml
+		if ! grep 'path: /etc/kubernetes/scheduler-policy-config.json' $dir/kube-scheduler.yaml; then
+			sed -i "/  volumes:/a \  - hostPath:\n      path: /etc/kubernetes/scheduler-policy-config.json\n      type: FileOrCreate\n    name: scheduler-policy-config" $dir/kube-scheduler.yaml
+		fi
 	else
 		public::common::log "Skip the kube-scheduler config, because it's already configured extender."
 	fi
