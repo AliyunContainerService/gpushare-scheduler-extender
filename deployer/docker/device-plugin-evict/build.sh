c6f8b8ca1594ba29c61017a1e6d235200cae9a82
cheyang <cheyang@163.com>
2019-04-17 16:26:46 +0800

    add tag

--- deployer/docker/device-plugin-evict/build.sh
+++ deployer/docker/device-plugin-evict/build.sh
@@ -7,3 +7,5 @@ cd $BASEDIR
 
 # docker build --no-cache -t $IMAGE -f $FILE $BASEDIR
 docker build -t registry.cn-hangzhou.aliyuncs.com/acs/device-plugin-evict:$TIMESTAMP .
+
+docker tag registry.cn-hangzhou.aliyuncs.com/acs/device-plugin-evict:$TIMESTAMP cheyang/device-plugin-evict:$TIMESTAMP
--- deployer/docker/schd-extender/build.sh
+++ deployer/docker/schd-extender/build.sh
@@ -7,3 +7,6 @@ cd $BASEDIR
 
 # docker build --no-cache -t $IMAGE -f $FILE $BASEDIR
 docker build -t registry.cn-hangzhou.aliyuncs.com/acs/schd-extender-deployer:$TIMESTAMP .
+
+docker tag registry.cn-hangzhou.aliyuncs.com/acs/schd-extender-deployer:$TIMESTAMP cheyang/schd-extender-deployer:$TIMESTAMP
+
