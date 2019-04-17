bf40e1e00db41e8dc4da1bed5f4832bc4ed35465
cheyang <cheyang@163.com>
2019-04-17 23:45:43 +0800

    add gpushare

--- deployer/docker/device-plugin-evict/dp-evict/dp-evict.sh
+++ deployer/docker/device-plugin-evict/dp-evict/dp-evict.sh
@@ -2,10 +2,10 @@
 set -xe
 
 if [ -d "/k8s-host" ]; then
-	rm -rf /k8s-host/usr/local/k8s-host
-	mkdir -p /k8s-host/usr/local/k8s-host
+	rm -rf /k8s-host/usr/local/dp-evict
+	mkdir -p /k8s-host/usr/local/dp-evict
 	cp -r /dp-evict/* /k8s-host/usr/local/dp-evict
 	chmod -R +x /k8s-host/usr/local/dp-evict
-	chroot /k8s-host /usr/local/k8s-host/dp-evict-on-host.sh "$@"
+	chroot /k8s-host /usr/local/dp-evict/dp-evict-on-host.sh "$@"
 	while sleep 3600; do :; done
 fi
