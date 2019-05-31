4372572db6ec9c711ac796f6aec04ba1a470bbcd
cheyang <cheyang@163.com>
2019-05-31 15:52:44 +0800

    update build.sh

--- deployer/docker/device-plugin-evict/build.sh
+++ deployer/docker/device-plugin-evict/build.sh
@@ -10,6 +10,7 @@ cd $BASEDIR
 
 # docker build --no-cache -t $IMAGE -f $FILE $BASEDIR
 docker build --network=host -t registry.cn-hangzhou.aliyuncs.com/acs/nvidia-device-plugin-evict:$TIMESTAMP .
+
 docker push registry.cn-hangzhou.aliyuncs.com/acs/nvidia-device-plugin-evict:$TIMESTAMP
 
 echo registry.cn-hangzhou.aliyuncs.com/acs/nvidia-device-plugin-evict:$TIMESTAMP
