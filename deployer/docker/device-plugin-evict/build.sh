13432f328ef68ebb9f48a06d2a94e8927118454b
root <manifoldyu@gmail.com>
2019-05-31 13:15:16 +0800

    add device-plugin-recover daemonSet

--- deployer/docker/device-plugin-evict/build.sh
+++ deployer/docker/device-plugin-evict/build.sh
@@ -9,6 +9,7 @@ TIMESTAMP=$(date +%Y%m%d%H%M)
 cd $BASEDIR
 
 # docker build --no-cache -t $IMAGE -f $FILE $BASEDIR
-docker build -t registry.cn-hangzhou.aliyuncs.com/acs/device-plugin-evict:$TIMESTAMP .
+docker build --network=host -t registry.cn-shanghai.aliyuncs.com/hy_base/nvidia-device-plugin-evict:$TIMESTAMP .
+docker push registry.cn-shanghai.aliyuncs.com/hy_base/nvidia-device-plugin-evict:$TIMESTAMP
 
-docker tag registry.cn-hangzhou.aliyuncs.com/acs/device-plugin-evict:$TIMESTAMP cheyang/device-plugin-evict:$TIMESTAMP
+echo registry.cn-shanghai.aliyuncs.com/hy_base/nvidia-device-plugin-evict:$TIMESTAMP
