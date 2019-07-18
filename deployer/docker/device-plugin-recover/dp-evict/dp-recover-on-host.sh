00baedb39a0e377039f9fe2ce1e0df5e0025d705
cheyang <cheyang@163.com>
2019-07-18 16:24:03 +0800

    Make recover script to check manifest exists. (#44)
    * fix recover script
    
    * fix recover script
    
    * fix recover script

--- deployer/docker/device-plugin-recover/dp-evict/dp-recover-on-host.sh
+++ deployer/docker/device-plugin-recover/dp-evict/dp-recover-on-host.sh
@@ -14,8 +14,12 @@ public::recover::gpu-device-plugin() {
     if [  -f $dir/nvidia-device-plugin.yml ]; then
 		public::common::log "Skip recovering nvidia-device-plugin.yml, because it already exist."
     else
-		mv -f $backup_dir/nvidia-device-plugin.yml $dir/nvidia-device-plugin.yml
-		public::common::log "Finish recovering nvidia-device-plugin.yml."
+    	if [  -f $backup_dir/nvidia-device-plugin.yml ]; then
+			cp -f $backup_dir/nvidia-device-plugin.yml $dir/nvidia-device-plugin.yml
+			public::common::log "Finish recovering nvidia-device-plugin.yml."
+		else
+			public::common::log "No nvidia-device-plugin.yml to recover."
+		fi	
 	fi
 }
 
